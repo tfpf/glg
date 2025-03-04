c13fc18d40681db59e256fdd6e639ae9366059e0
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-03-04 23:30:04 +0530

    Load repository

--- src/glog/glog.py
+++ src/glog/glog.py
@@ -1,10 +1,38 @@
-from pygit2 import Repository
+import pygit2
+import argparse
 
-repo = Repository("../../../dotfiles")
-for ref in repo.references:
-    ref = repo.references.get(ref)
-    print(ref.shorthand, ref.resolve().target)
-# for commit in repo.walk(repo.head.target):
-for commit in repo.walk(repo.references.get("refs/remotes/origin/git-difftool").target):
-    # print(dir(commit))
-    print(commit.id, commit.parents)
+
+class GitRepositoryLogVisualiser:
+    def __init__(self, cfg: argparse.Namespace):
+        self._repository = pygit2.Repository(".")
+        if cfg.all:
+            self._references = [self._repository.references.get(reference) for reference in self._repository.references]
+        else:
+            self._references = [self._repository.head]
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(repository={self._repository.path}, references={self._references})"
+
+    def visualise(self):
+        pass
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--all", action="store_true", help="show all commits from refs")
+    cfg = parser.parse_args()
+
+    visualiser = GitRepositoryLogVisualiser(cfg)
+    visualiser.visualise()
+
+if __name__ == "__main__":
+    main()
+
+
+# repo = Repository("../../../dotfiles")
+# for ref in repo.references:
+#     ref = repo.references.get(ref)
+#     print(ref.shorthand, ref.resolve().target)
+# # for commit in repo.walk(repo.head.target):
+# for commit in repo.walk(repo.references.get("refs/remotes/origin/git-difftool").target):
+#     # print(dir(commit))
+#     print(commit.id, commit.parents)
