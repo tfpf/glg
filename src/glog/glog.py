79827ec59bd1077cbf1f6d6f520f34f5b8026ef5
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-03-10 18:09:55 +0530

    WIP commit

--- src/glog/glog.py
+++ src/glog/glog.py
@@ -9,14 +9,17 @@ class GitRepositoryLogVisualiser:
             self._references = [self._repository.references.get(reference).resolve() for reference in self._repository.references]
         else:
             self._references = [self._repository.head.resolve()]
-        for reference in self._references:
-            print(reference.target, reference.shorthand)
 
     def __repr__(self):
         return f"{self.__class__.__name__}(repository={self._repository.path}, references={self._references})"
 
     def visualise(self):
-        pass
+        for reference in self._references:
+            self._visualise_reference(reference)
+
+    def _visualise_reference(self, reference: pygit2.Reference):
+        print(reference.target, self._repository.get(reference.target))
+
 
 def main():
     parser = argparse.ArgumentParser()
